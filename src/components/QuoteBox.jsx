1b20117888189b6f625999c55cf1eadd4f1a41e4
Derian Melvin <derianmelvin88@gmail.com>
2021-01-12 15:23:12 +0700

    - Import axios - Add componentDidMount() - Add method 'getNewQuote' - Change 'id' of <main> to #container - Add <div> with 'id' #container-quote - Add <div> with 'id' #container button - Re-arrange existing content into each <div> container - Get state value for #text & #author - Add 'onClick' for #new-quote

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,21 +1,51 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import axios from "axios";
 
 class QuoteBox extends Component {
-  state = { 
-    quote: '',
-    author: ''
+  state = {
+    quote: "",
+    author: "",
+  };
+
+  componentDidMount() {
+    axios.get("http://quotes.stormconsultancy.co.uk/random.json").then((res) =>
+      this.setState({
+        quote: res.data.quote,
+        author: res.data.author,
+      })
+    );
   }
+
+  getNewQuote = () => {
+    axios.get("http://quotes.stormconsultancy.co.uk/random.json").then((res) =>
+      this.setState({
+        quote: res.data.quote,
+        author: res.data.author,
+      })
+    );
+  };
 
   render() {
     return (
-      <main id="wrapper">
-        <div id="text"></div>
-        <div id="author"></div>
-        <button id="new-quote">New quote</button>
-        <a id="tweet-quote" href="twitter.com/intent/tweet">Tweet</a>
+      <main id="container">
+        <div id="quote-box">
+          <div id="container-quote">
+            <h1 id="text">{'" ' + this.state.quote + ' "'}</h1>
+            <h2 id="author">{'- ' + this.state.author + ' -'}</h2>
+          </div>
+
+          <div id="container-button">
+            <button id="new-quote" onClick={this.getNewQuote}>
+              New quote
+            </button>
+            <a id="tweet-quote" href="twitter.com/intent/tweet">
+              Tweet
+            </a>
+          </div>
+        </div>
       </main>
     );
   }
 }
- 
+
 export default QuoteBox;
