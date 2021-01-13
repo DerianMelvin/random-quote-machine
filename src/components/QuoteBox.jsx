d59bce1be8c4075d2e624b89641fc5abe088db11
Derian Melvin <derianmelvin88@gmail.com>
2021-01-13 13:14:34 +0700

    - Change request link in axios.get - Add const randomQuote - Modify setState value

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -8,21 +8,23 @@ class QuoteBox extends Component {
   };
 
   componentDidMount() {
-    axios.get("http://quotes.stormconsultancy.co.uk/random.json").then((res) =>
+    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then((res) => {
+      const randomQuote = Math.floor(Math.random() * res.data.quotes.length);
       this.setState({
-        quote: res.data.quote,
-        author: res.data.author,
-      })
-    );
+        quote: res.data.quotes[randomQuote].quote,
+        author: res.data.quotes[randomQuote].author,
+      });
+    })
   }
 
   getNewQuote = () => {
-    axios.get("http://quotes.stormconsultancy.co.uk/random.json").then((res) =>
+    axios.get("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json").then((res) => {
+      const randomQuote = Math.floor(Math.random() * res.data.quotes.length);
       this.setState({
-        quote: res.data.quote,
-        author: res.data.author,
-      })
-    );
+        quote: res.data.quotes[randomQuote].quote,
+        author: res.data.quotes[randomQuote].author,
+      });
+    });
   };
 
   render() {
